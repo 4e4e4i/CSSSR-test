6a28430ba6de6104866b071bc4c26df800f5d178
Zozulya Vasiliy <zazevang@gmail.com>
2018-12-18 14:08:25 +0300

    тестовое для CSSSR вёрстка

---
+++ app/scripts/app.js
@@ -0,0 +1,22 @@
+import svg4everybody from 'svg4everybody';
+import $ from 'jquery';
+
+$(() => {
+	svg4everybody();
+});
+
+const sliderItems = document.querySelectorAll('.field-range__value');
+const sliderButton = document.querySelector('.field-range__track-btn');
+
+Array.prototype.slice.call(sliderItems).forEach(function (item, index) {
+	item.addEventListener('click', function () {
+		const isLast = index + 1 === sliderItems.length;
+		let itemPosition = item.offsetLeft;
+
+		if (isLast) {
+			itemPosition += item.offsetWidth;
+		}
+
+		sliderButton.style.left = itemPosition + 'px';
+	});
+});
